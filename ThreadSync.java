0f634042682962612015b07b67174809177fd31e
rubyaj24 <amaljithmvinod@gmail.com>
2024-11-12 00:19:58 +0530

    nothing much

---
+++ ThreadSync.java
@@ -0,0 +1,33 @@
+class MyThread extends Thread {
+    public MyThread(String name) {
+        super(name);
+    }
+
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(Thread.currentThread().getName() + " - Priority: " + Thread.currentThread().getPriority() + " - " + i);
+            try {
+                Thread.sleep(500); // Sleep for 500 milliseconds
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+public class ThreadSync {
+    public static void main(String[] args) {
+        MyThread thread1 = new MyThread("Thread 1");
+        MyThread thread2 = new MyThread("Thread 2");
+        MyThread thread3 = new MyThread("Thread 3");
+
+        // Set thread priorities
+        thread1.setPriority(Thread.MIN_PRIORITY); // Priority 1
+        thread2.setPriority(Thread.NORM_PRIORITY); // Priority 5
+        thread3.setPriority(Thread.MAX_PRIORITY); // Priority 10
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+    }
+}
